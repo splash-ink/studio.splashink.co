b65ba799cbb3d57d7e8aeb1e81e27cb51e6a973b
Jorge Dacosta <jorgemsdc@mail.com>
2019-03-29 14:04:01 +0000

    declaring onBoard comp. as main loads page 🚀

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from '@core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OnboardComponent } from './onboard/onboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnboardComponent
   ],
   imports: [
     BrowserModule,
