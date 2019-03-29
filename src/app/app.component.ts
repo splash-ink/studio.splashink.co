ef66c0bae26a3b55d3ee33c35a9f07223550e0a1
Jorge Dacosta <jorgemsdc@mail.com>
2019-03-29 01:10:36 +0000

    chore: adding class to body on load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'ph-spl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Photos';
+export class AppComponent implements OnInit {
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+    ngOnInit(): void {
+      const c = this.document.body.classList.contains('left-nav-sidebar');
+
+      if (!c) {
+        this.document.body.classList.add('left-nav-sidebar');
+      }
+    }
 }
