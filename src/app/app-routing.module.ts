9ca3f3286d3fc53b591ceb6369c731c4f9023821
Jorge Dacosta <jorgemsdc@mail.com>
2019-03-29 14:02:33 +0000

    chore: config initial router path comp.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OnboardComponent } from './onboard/onboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: OnboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
