7b43470db35cfec7d8ed163e8bf07385330008e0
Jorge Dacosta <jorgemsdc@mail.com>
2019-05-30 20:24:20 +0100

    init onBoard component

---
+++ src/app/onboard/onboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ph-spl-onboard',
+  templateUrl: './onboard.component.html',
+})
+export class OnboardComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+}
