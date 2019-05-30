473b6e0b2d9fd42e2e5a3ffa6d476e371000d5fe
Jorge Dacosta <jorgemsdc@mail.com>
2019-05-30 20:23:43 +0100

    declaring banner-widget for OnboardComponent 🍻

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { CoreModule } from '@core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OnboardComponent } from './onboard/onboard.component';
+import { BannerWidgetComponent } from './onboard/banner-widget/banner-widget.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OnboardComponent
+    OnboardComponent,
+    BannerWidgetComponent
   ],
   imports: [
     BrowserModule,
