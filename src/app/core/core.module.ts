7b923f4ce743776e577f3abe8261738e276bec7a
Jorge Dacosta <jorgemsdc@mail.com>
2019-05-31 15:21:01 +0100

    declaring footer as part of this module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,14 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { throwIfAlreadyLoaded } from './module-import-guard';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [NavComponent],
+  declarations: [NavComponent, FooterComponent],
   imports: [
     CommonModule
   ],
-  exports: [NavComponent]
+  exports: [NavComponent, FooterComponent]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
