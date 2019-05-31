29831ae00468f227643574a871740d49af583c43
Jorge Dacosta <jorgemsdc@mail.com>
2019-05-31 15:20:40 +0100

    chore: add FooterComponent

---
+++ src/app/core/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ph-spl-footer',
+  templateUrl: './footer.component.html',
+  styles: []
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
