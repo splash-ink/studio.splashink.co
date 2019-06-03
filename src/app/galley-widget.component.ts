573a5c237d1113622a30d9163bf30e45fd5979e4
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-03 20:55:18 +0100

    chore: add GalleryWidget for home 🍻

---
+++ src/app/galley-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ph-spl-galley-widget',
+  template: './galley-widget.component.html',
+  styles: []
+})
+export class GalleyWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
