43618d234d8b0dc0be9c1cf455fd0d6b5a85ebe1
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-03 20:55:48 +0100

    declaring GalleryWidget as child of module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OnboardComponent } from './onboard/onboard.component';
 import { BannerWidgetComponent } from './onboard/banner-widget/banner-widget.component';
+import { GalleyWidgetComponent } from './galley-widget.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OnboardComponent,
-    BannerWidgetComponent
+    BannerWidgetComponent,
+    GalleyWidgetComponent
   ],
   imports: [
     BrowserModule,
