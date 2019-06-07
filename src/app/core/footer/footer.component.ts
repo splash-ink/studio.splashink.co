c2f2055135552bf17e32f6099b01031f70e92d73
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-07 22:38:23 +0100

    Link to special footer CSS file

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ph-spl-footer',
   templateUrl: './footer.component.html',
-  styles: []
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
 
+  year = Date.now();
   constructor() { }
 
   ngOnInit() {
