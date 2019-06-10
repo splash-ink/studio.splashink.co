edb24ef52d12c681c0483af8ac24c9df7cf037aa
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-10 17:26:52 +0100

    🧷 Gallery Widget

---
+++ src/app/onboard/gallery-widget/gallery-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ph-spl-gallery-widget',
+  templateUrl: './gallery-widget.component.html',
+  styles: []
+})
+export class GalleryWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
