e3195922ddbdcb1274f2783a4e01bd9ff1a69362
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-10 14:40:58 +0100

    fix-typo (#2): refactor banner comp. to match prototype expections

--- src/app/onboard/banner-widget/banner-widget.component.ts
+++ src/app/onboard/banner-widget/banner-widget.component.ts
@@ -4,23 +4,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ph-spl-banner-widget',
   templateUrl: './banner-widget.component.html',
-  styles: ['.onBoardSection { background-position: 0px 0px; visibility: visible; animation-name: fadeIn; }']
+  styleUrls: ['./banner-widget.component.css']
 })
 export class BannerWidgetComponent implements OnInit {
 
   title = 'Nós combinamos design, pensamento e\
   técnica.';
 
-  body = 'We are a full-service digital agency. Everything you could possibly want it to do and not only that,\
-  beautifully carefully designed.';
-
-  imgUrl = 'https://www.themezaa.com/html/pofo/images/slider-01.jpg';
+  imgUrl = 'assets/images/landing-cover.jpg';
 
   constructor(private sanitizer: DomSanitizer) { }
 
-  getWCBg() {
-    const style = `background-image: url(${this.imgUrl});`;
+  setOnboardImage() {
+    if (typeof(this.imgUrl) === undefined) { return; }
 
+    const style = `background-image: url(${this.imgUrl});`;
     return this.sanitizer.bypassSecurityTrustStyle(style);
   }
 
