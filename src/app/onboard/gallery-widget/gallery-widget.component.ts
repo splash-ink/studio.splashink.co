d659a677b7f2486b9497d8a829e42837eeb6deb9
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-11 20:57:47 +0100

    Expecting incoming data from parent

--- src/app/onboard/gallery-widget/gallery-widget.component.ts
+++ src/app/onboard/gallery-widget/gallery-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ph-spl-gallery-widget',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryWidgetComponent implements OnInit {
 
+  @Input() data;
   constructor() { }
 
   ngOnInit() {
