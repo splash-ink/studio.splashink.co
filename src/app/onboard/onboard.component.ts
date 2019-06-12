4bd8fa0b3fda795b915b249f38b82dbf4d125e5d
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-12 21:35:42 +0100

    Importing forged data tto test

--- src/app/onboard/onboard.component.ts
+++ src/app/onboard/onboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Galleries } from '../data';
 
 @Component({
   selector: 'ph-spl-onboard',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnboardComponent implements OnInit {
 
+  data = Galleries;
   ngOnInit() {
   }
 
