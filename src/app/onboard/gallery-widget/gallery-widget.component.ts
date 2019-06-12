a4fd97fe0bbdde2c9976d601735fba76a463b539
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-12 21:37:42 +0100

    Expecting incoming data with GalleryModel shape; Apply tags at style classes list to engage with filter

--- src/app/onboard/gallery-widget/gallery-widget.component.ts
+++ src/app/onboard/gallery-widget/gallery-widget.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject, AfterViewInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { GalleryModel } from '@gallery/gallery.model';
 
 @Component({
   selector: 'ph-spl-gallery-widget',
   templateUrl: './gallery-widget.component.html',
   styles: []
 })
-export class GalleryWidgetComponent implements OnInit {
+export class GalleryWidgetComponent implements OnInit, AfterViewInit {
 
-  @Input() data;
-  constructor() { }
+  @Input() data: GalleryModel;
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.data.tags.forEach(tag => {
+      this.document.getElementById(`${this.data.name}`).classList.add(tag);
+    });
   }
 
 }
