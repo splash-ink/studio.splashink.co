0dd09ff7ff993ed06dc133f2d21862383ae92e48
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-12 21:36:06 +0100

    Forged data

---
+++ src/app/data.ts
@@ -0,0 +1,28 @@
+import { GalleryModel, ImageModel } from './gallery/gallery.model';
+
+export const Galleries: GalleryModel [] = [
+    {
+        name: 'Get to know the world',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        thumbnail: 'https://www.themezaa.com/html/pofo/images/blog-page-03-img5.jpg',
+        tags: ['web', 'branding']
+    },
+    {
+        name: 'The most beautiful sky',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        thumbnail: 'https://www.themezaa.com/html/pofo/images/blog-page-03-img6.jpg',
+        tags: ['photography', 'web']
+    },
+    {
+        name: 'I can\'t change the rule',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        thumbnail: 'https://www.themezaa.com/html/pofo/images/blog-page-03-img7.jpg',
+        tags: ['design', 'branding']
+    },
+    {
+        name: 'Happiness is something',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        thumbnail: 'https://www.themezaa.com/html/pofo/images/blog-page-03-img8.jpg',
+        tags: ['advertising']
+    }
+];
