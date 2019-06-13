73ea2cce232eedfd207877b71a13cf62af6b0af8
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-13 16:35:01 +0100

    filtering RWE tags

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,28 +1,30 @@
 import { GalleryModel, ImageModel } from './gallery/gallery.model';
 
-export const Galleries: GalleryModel [] = [
+export const TAGS: string [] = ['web', 'advertising', 'branding', 'design', 'photography'];
+
+export const GALLERIES: GalleryModel [] = [
     {
         name: 'Get to know the world',
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
         thumbnail: 'https://www.themezaa.com/html/pofo/images/blog-page-03-img5.jpg',
-        tags: ['web', 'branding']
+        tags: ['marca', 'design']
     },
     {
         name: 'The most beautiful sky',
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
         thumbnail: 'https://www.themezaa.com/html/pofo/images/blog-page-03-img6.jpg',
-        tags: ['photography', 'web']
+        tags: ['ilustracao']
     },
     {
         name: 'I can\'t change the rule',
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
         thumbnail: 'https://www.themezaa.com/html/pofo/images/blog-page-03-img7.jpg',
-        tags: ['design', 'branding']
+        tags: ['design', 'produto']
     },
     {
         name: 'Happiness is something',
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
         thumbnail: 'https://www.themezaa.com/html/pofo/images/blog-page-03-img8.jpg',
-        tags: ['advertising']
+        tags: ['fotografia']
     }
 ];
