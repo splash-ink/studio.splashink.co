149f0f1465d1f66206883f25fb84451eb54d280b
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-13 16:34:19 +0100

    style for filter-text & forged galleries

--- src/app/onboard/filter-widget/filter-widget.component.ts
+++ src/app/onboard/filter-widget/filter-widget.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TAGS, GALLERIES } from '../../data';
 
 @Component({
   selector: 'ph-spl-filter-widget',
   templateUrl: './filter-widget.component.html',
-  styles: []
+  styles: [
+    `
+    .filter-text {
+      font-weight: 600;
+      font-size: 12px;
+      border-bottom: none !important;
+    }
+    `
+  ]
 })
 export class FilterWidgetComponent implements OnInit {
 
+  galleries = GALLERIES;
   constructor() { }
 
   ngOnInit() {
