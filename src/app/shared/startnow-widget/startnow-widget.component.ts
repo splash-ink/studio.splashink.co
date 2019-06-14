758849ba846b301a61ffec1f77cf15087f0bdc70
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-14 13:59:38 +0100

    Making widget dynamic

---
+++ src/app/shared/startnow-widget/startnow-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ph-spl-startnow-widget',
+  templateUrl: './startnow-widget.component.html',
+  styles: []
+})
+export class StartnowWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
