e37ec933119b5bf80ba940f2c2cfe7e29ecc4a2b
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-14 02:52:24 +0100

    Remove unused methods

--- src/app/onboard/gallery-widget/gallery-widget.component.ts
+++ src/app/onboard/gallery-widget/gallery-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Inject, AfterViewInit } from '@angular/core';
+import { Component, Input, Inject, AfterViewInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { GalleryModel } from '@gallery/gallery.model';
 
@@ -7,13 +7,10 @@ import { GalleryModel } from '@gallery/gallery.model';
   templateUrl: './gallery-widget.component.html',
   styles: []
 })
-export class GalleryWidgetComponent implements OnInit, AfterViewInit {
+export class GalleryWidgetComponent implements AfterViewInit {
 
   @Input() data: GalleryModel;
   constructor(@Inject(DOCUMENT) private document: Document) { }
-
-  ngOnInit() {
-  }
 
   ngAfterViewInit() {
     this.data.tags.forEach(tag => {
