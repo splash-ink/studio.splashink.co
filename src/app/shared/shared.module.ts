2a84bacc6677a0bab06f114235dc79ab4cbf18a0
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-14 13:58:36 +0100

    Import RouterModule dependencies; Declaring & Exporting widgets

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { StartnowWidgetComponent } from './startnow-widget/startnow-widget.component';
+
+const comps = [
+  StartnowWidgetComponent
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [comps],
+  exports: [comps],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
