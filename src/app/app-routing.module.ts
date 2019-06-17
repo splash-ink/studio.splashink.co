57cefda04ad44c3ae37ad0e3e2477267bf9e8513
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-17 12:12:22 +0100

    Lazy-loading `Blog` Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { OnboardComponent } from './onboard/onboard.component';
 
 const routes: Routes = [
+  {
+    path: 'blog',
+    loadChildren: () => import('@blog/blog.module').then(m => m.BlogModule)
+  },
   { path: '', component: OnboardComponent }
 ];
 
