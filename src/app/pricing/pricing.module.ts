bffcc6316ace1aa0e9fa9f1bd933f11c81f83e3a
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-19 13:33:45 +0100

    chore(feat.): declaring Accordion widget

--- src/app/pricing/pricing.module.ts
+++ src/app/pricing/pricing.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PricingRoutingModule } from './pricing-routing.module';
 import { PricingComponent } from './pricing.component';
 import { PriceWidgetComponent } from './price-widget/price-widget.component';
+import { AccordionWidgetComponent } from './accordion-widget/accordion-widget.component';
 
 @NgModule({
-  declarations: [PricingComponent, PriceWidgetComponent],
+  declarations: [PricingComponent, PriceWidgetComponent, AccordionWidgetComponent],
   imports: [
     CommonModule,
     PricingRoutingModule
