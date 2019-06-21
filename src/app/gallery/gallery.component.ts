fc8d5057bcd31a704748e43f170fed317a33d811
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-21 22:55:51 +0100

    Setup for magnific-popup

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'magnific-popup';
+
 
 @Component({
   selector: 'ph-spl-gallery',
   templateUrl: './gallery.component.html',
   styles: []
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    $('.image-link').magnificPopup({
+      type: 'image',
+      mainClass: 'mfp-fade',
+      image: {
+        titleSrc: 'title',
+        verticalFit: true,
+        tError: '<a href="%url%">A imagem</a> não pode ser carregada.'
+      },
+      gallery: {
+        enabled: true,
+      }
+    });
   }
 
 }
