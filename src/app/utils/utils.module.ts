468ed0773c83cee03be3b8a2ad65f68b863f4afe
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-26 23:12:59 +0100

    Utilities Module

---
+++ src/app/utils/utils.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { FirestoreDataService } from './firestore-data.service';
+
+@NgModule({
+  providers: [
+    FirestoreDataService
+  ]
+})
+export class UtilsModule { }
