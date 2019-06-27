f72ffdfea4fa669c248ab875a0689d65c54582b9
Jorge Dacosta <jorgemsdc@mail.com>
2019-06-27 17:31:20 +0100

    Remove unnecessary module

--- src/app/utils/utils.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { FirestoreDataService } from './firestore-data.service';
-
-@NgModule({
-  providers: [
-    FirestoreDataService
-  ]
-})
-export class UtilsModule { }
