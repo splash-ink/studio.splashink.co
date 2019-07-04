7bbc7e3e2b8bf7e16d0493caa2ed1026f029aea6
Jorge Dacosta <jorgemsdc@mail.com>
2019-07-04 20:37:43 +0100

    Importing shared libs

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { PostComponent } from './post/post.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [PostComponent],
   imports: [
     CommonModule,
-    BlogRoutingModule
+    BlogRoutingModule,
+    SharedModule
   ]
 })
 export class BlogModule { }
