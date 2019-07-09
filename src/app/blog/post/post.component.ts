d4f2471f8b8cb137715811ef3a7385646916d785
Jorge Dacosta <jorgemsdc@mail.com>
2019-07-09 21:25:42 +0100

    Business logic for Post details

--- src/app/blog/post/post.component.ts
+++ src/app/blog/post/post.component.ts
@@ -1,31 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FirestoreDataService } from '@core/firestore-data.service';
+import { Subscription } from 'rxjs';
+import { PostModel } from './post.model';
 
 @Component({
   selector: 'ph-spl-post',
   templateUrl: './post.component.html',
-  styles: []
-})
-export class PostComponent implements OnInit {
+  styles: [
+    `
+      .post-bg-opacity {
+        background-color: rgba(1, 14, 22, 0.8);
+      }
 
-  startWith: string;
-  text = `Georgia Dacosta bibendum arcu vitae elementum curabitur vitae. Sed arcu non odio Euismod. Pulvinar pellentesque habitant
-  morbi tristique. Vitae turpis massa sed elementum. Purus in massa tempor nec. Nisl purus in mollis nunc sed id semper. Enim sit amet vene\
-  natis urna cursus eget nunc scelerisque. Porttitor massa id neque aliquam vestibulum morbi blandit. Enim facilisis gravida neque convalli
-  s a cras semper auctor. Quis viverra nibh cras pulvinar. Felis eget nunc lobortis mattis aliquam faucibus purus in. Id neque aliquam vest\
-  ibulum morbi blandit cursus. Neque ornare aenean euismod elementum nisi. Sed risus ultricies tristique nulla.
-  Pulvinar mattis nunc sed blandit libero. Accumsan lacus vel facilisis volutpat. Nisl condimentum id venenatis a condimentum vitae sapien.
-  Lobortis mattis aliquam faucibus purus. Aliquam ut porttitor leo a diam. Suspendisse sed nisi lacus sed viverra tellus in hac habitasse.
-  Sed vulputate odio ut enim blandit volutpat. Metus dictum at tempor commodo ullamcorper a lacus. Interdum velit laoreet id donec ultrices
-  tincidunt arcu. In hac habitasse platea dictumst. Id diam maecenas ultricies mi eget mauris pharetra et. Venenatis cras sed felis eget ve
-  lit aliquet. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Est ante in nibh mauris cursus. Turpis in eu mi bibendum
-  neque egestas. Quis enim lobortis scelerisque fermentum. Diam donec adipiscing tristique risus nec feugiat. Tincidunt arcu non sodales
-  neque sodales ut etiam sit amet. Facilisi nullam vehicula ipsum a arcu cursus vitae. Aliquet risus feugiat in ante metus.
-  `;
-  constructor() { }
+      .txt-to-upper-case {
+        text-transform: uppercase;
+      }
+    `
+  ]
+})
+export class PostComponent implements OnInit, OnDestroy {
+
+  doc$: PostModel;
+  helper: Subscription;
+  /// alias for async fields
+  startWith = '';
+  constructor(private fds: FirestoreDataService, private sanitizer: DomSanitizer) { }
+
+  syncBg() {
+    return this.sanitizer.bypassSecurityTrustStyle(`
+    background-image: url('${this.doc$.thumbnail}');
+    visibility: visible;
+    animation-name: fadeIn;
+    padding: 40px 0;`);
+  }
 
   ngOnInit() {
-    this.startWith = this.text.charAt(0);
-    this.text = this.text.substring(1);
+    this.helper = this.fds.doc$<PostModel>('blog/LYE0cQ2QPfMz3svXiGfW')
+    .subscribe(payload => {
+      this.doc$ = payload;
+
+      this.startWith = this.doc$.body.charAt(0);
+      this.doc$.body = this.doc$.body.substring(1);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.helper.unsubscribe();
   }
 
 }
