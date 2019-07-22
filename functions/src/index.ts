8de8d6a350acc4960b00e75511755844531cd9e1
Jorge Dacosta <jorgemsdc@mail.com>
2019-07-22 23:01:33 +0100

    Performing event to send firestore docs via GET request

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,24 @@
 import * as functions from 'firebase-functions';
+import * as cors from 'cors';
+import * as express from 'express';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+/// get elements & init app
+admin.initializeApp();
+const db = admin.firestore();
+
+/// setup express application
+const app = express();
+app.use(cors({ origin: true }));
+
+//#region CALLS
+
+    app.get('/galleries', async (request, response) => {
+        const data = await db.collection('galleries').get();
+        return response.send(data.docs);
+    })
+
+//#endregion
+
+/// assigning express config to 🔥 Cloud Functions
+export const api = functions.https.onRequest(app);
