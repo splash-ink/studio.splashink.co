ea67b3ab3bbb338633ba35fc19d08ab48ff027be
x <x@X>
2019-08-27 23:38:07 +0100

    chore: add generateTags method to handle missing tags

--- src/app/core/seo.service.ts
+++ src/app/core/seo.service.ts
@@ -7,4 +7,30 @@ import { Meta, Title } from '@angular/platform-browser';
 export class SeoService {
 
   constructor(private meta: Meta, private title: Title) { }
+
+  generateTags(config) {
+    config = {
+      title: 'Splash Ink :: Estúdio Digital',
+      description: 'Put your site description here.',
+      image: 'https://domain-to-picture.com',
+      slug: '',
+      ...config
+    };
+
+    this.title.setTitle(config.title);
+
+    this.meta.updateTag({ name: 'twitter:card', content: 'summary' });
+    this.meta.updateTag({ name: 'twitter:site', content: '@splashink_' });
+    this.meta.updateTag({ name: 'twitter:title', content: config.title });
+    this.meta.updateTag({ name: 'twitter:description', content: config.description });
+    this.meta.updateTag({ name: 'twitter:image', content: config.image });
+
+    this.meta.updateTag({ property: 'og:type', content: 'article' });
+    this.meta.updateTag({ property: 'og:site_name', content: 'splashink' });
+    this.meta.updateTag({ property: 'og:title', content: config.title });
+    this.meta.updateTag({ property: 'og:description', content: config.description });
+    this.meta.updateTag({ property: 'og:image', content: config.image });
+    this.meta.updateTag({ property: 'og:url', content: `https://studio.splashink.co/${config.slug}` });
+
+  }
 }
