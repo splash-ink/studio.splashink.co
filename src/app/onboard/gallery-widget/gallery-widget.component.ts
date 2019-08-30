406aa70598eb599e2aeb91ce7ec610c054811232
Jorge Dacosta <jorgedacostaza@gmail.com>
2019-08-30 21:52:41 +0100

    insert style directly from comp

--- src/app/onboard/gallery-widget/gallery-widget.component.ts
+++ src/app/onboard/gallery-widget/gallery-widget.component.ts
@@ -4,7 +4,19 @@ import { GalleryModel } from '@gallery/gallery.model';
 @Component({
   selector: 'ph-spl-gallery-widget',
   templateUrl: './gallery-widget.component.html',
-  styles: []
+  styles: [`
+    .gallery-item {
+      visibility: visible;
+      animation-name: fadeInUp;
+      /* position: absolute; */
+      /* left: 0%; */
+      /* top: 0px; */
+    }
+
+    .gallery-item {
+      display: inline-block;
+    }
+  `]
 })
 export class GalleryWidgetComponent implements OnInit {
 
