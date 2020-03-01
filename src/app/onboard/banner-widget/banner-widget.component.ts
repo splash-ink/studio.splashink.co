7b22369f647a606a10c48d38a02195a6d680cc4a
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-01 15:35:29 +0100

    fix: remove all unworking code

--- src/app/onboard/banner-widget/banner-widget.component.ts
+++ src/app/onboard/banner-widget/banner-widget.component.ts
@@ -1,28 +1,11 @@
-import { DomSanitizer } from '@angular/platform-browser';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ds-banner-widget',
   templateUrl: './banner-widget.component.html',
   styleUrls: ['./banner-widget.component.css']
 })
-export class BannerWidgetComponent implements OnInit {
+export class BannerWidgetComponent {
 
-  title = 'Nós combinamos design, pensamento e\
-  técnica.';
-
-  imgUrl = 'assets/images/landing-cover.jpg';
-
-  constructor(private sanitizer: DomSanitizer) { }
-
-  setOnboardImage() {
-    if (typeof(this.imgUrl) === undefined) { return; }
-
-    const style = `background-image: url(${this.imgUrl});`;
-    return this.sanitizer.bypassSecurityTrustStyle(style);
-  }
-
-  ngOnInit() {
-  }
-
+  constructor() { }
 }
