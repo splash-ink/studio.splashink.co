525ee35ee54e3678f19628f67e4d2fa42a5ccd87
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-16 13:45:46 +0100

    fix: broken import path

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { PostComponent } from './post/post.component';
-import { SharedModule } from '@shared/shared.module';
+import { SharedModule } from '@studio/shared';
 
 @NgModule({
   declarations: [PostComponent],
