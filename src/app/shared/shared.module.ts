e77ff618149027bd4b95ee2dfc93ab6a07d09a92
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-16 14:07:29 +0100

    clean: remove unnecessary comp from mod

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { StartnowComponent } from './startnow/startnow.component';
 import { MdToHtmlPipe } from './md-to-html.pipe';
 
-const comps = [
-  StartnowComponent,
+const utils = [
   MdToHtmlPipe
 ];
 
 @NgModule({
-  declarations: [...comps],
-  exports: [...comps],
+  declarations: [...utils],
+  exports: [...utils],
   imports: [
     CommonModule,
     RouterModule
