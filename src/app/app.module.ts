7bcbe4ffcb4b5570c352c1ae9d2d5691497b6f67
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-16 14:00:12 +0100

    clean: let code more readable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,28 @@ import { QuicklinkModule } from 'ngx-quicklink';
 import { CoreModule } from '@studio/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { OnboardComponent } from './onboard/onboard.component';
 import { BannerComponent } from './onboard/banner/banner.component';
 import { GalleryComponent } from './onboard/gallery/gallery.component';
 import { FilterComponent } from './onboard/filter/filter.component';
 import { PartnerComponent } from './onboard/partner/partner.component';
+
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '@environments/environment';
 
+const comps = [
+  AppComponent,
+  OnboardComponent,
+  BannerComponent,
+  GalleryComponent,
+  FilterComponent,
+  PartnerComponent,
+];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    OnboardComponent,
-    BannerComponent,
-    GalleryComponent,
-    FilterComponent,
-    PartnerComponent,
-  ],
+  declarations: [ ...comps ],
   imports: [
     BrowserModule,
     AppRoutingModule,
