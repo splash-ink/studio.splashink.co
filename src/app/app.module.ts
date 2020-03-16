43d5d0b475dce0e93bdbf87c1eab2fd592db3a05
Jorge Dacosta <jorgeMSDC@mail.com>
2020-03-16 13:45:33 +0100

    fix: remove shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { BannerComponent } from './onboard/banner/banner.component';
 import { GalleryComponent } from './onboard/gallery/gallery.component';
 import { FilterComponent } from './onboard/filter/filter.component';
 import { PartnerComponent } from './onboard/partner/partner.component';
-import { SharedModule } from '@shared/shared.module';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '@environments/environment';
@@ -25,7 +24,6 @@ import { environment } from '@environments/environment';
   ],
   imports: [
     BrowserModule,
-    SharedModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule.enablePersistence(),
