16c4831634057b021df1096242b3b9b40c40f0e3
Jorge Dacosta <jorgeMSDC@mail.com>
2020-04-14 22:32:14 +0100

    chore(onboard): add onboard-css file to styleUrls array

--- src/app/onboard/onboard.component.ts
+++ src/app/onboard/onboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './onboard.component.html',
+  styleUrls: ['./onboard.component.css']
 })
 export class OnboardComponent {
 
