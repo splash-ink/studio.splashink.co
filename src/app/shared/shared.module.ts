6f6bf19442dc03ce8ddd3f4e3f2b612083f13469
Jorge Dacosta <jorgeMSDC@mail.com>
2020-04-15 16:20:39 +0100

    chore(shared): add gallery comp to shared comps array

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MdToHtmlPipe } from './md-to-html.pipe';
+import { GalleryComponent } from './gallery/gallery.component';
 
 const utils = [
   MdToHtmlPipe
 ];
 
+const comps = [
+  GalleryComponent
+]
+
+const shared = [
+  ...utils,
+  ...comps,
+]
 @NgModule({
-  declarations: [...utils],
-  exports: [...utils],
+  declarations: [
+    ...shared
+  ],
+  exports: [
+    ...shared
+  ],
   imports: [
     CommonModule,
     RouterModule
