3153f76da3d05877631bc1b555a948799428898c
Jorge Dacosta <jorgeMSDC@mail.com>
2020-04-15 16:52:24 +0100

    refactor(app): rem firebase modules & onboard peers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { QuicklinkModule } from 'ngx-quicklink';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from '@studio/core';
+import { QuicklinkModule } from 'ngx-quicklink';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { OnboardComponent } from './onboard/onboard.component';
-import { BannerComponent } from './onboard/banner/banner.component';
-import { GalleryComponent } from './onboard/gallery/gallery.component';
-import { FilterComponent } from './onboard/filter/filter.component';
-import { PartnerComponent } from './onboard/partner/partner.component';
-
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '@environments/environment';
-
-const comps = [
-  AppComponent,
-  OnboardComponent,
-  BannerComponent,
-  GalleryComponent,
-  FilterComponent,
-  PartnerComponent,
-];
-
 @NgModule({
-  declarations: [ ...comps ],
+  declarations: [ AppComponent ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     QuicklinkModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule.enablePersistence(),
-    CoreModule
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
