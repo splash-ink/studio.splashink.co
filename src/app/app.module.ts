fca305955feb5626ef7d4688eaf709cc5ade0ff2
Jorge Dacosta <jorgeMSDC@mail.com>
2020-04-17 23:44:46 +0100

    chore(app): set locale_id to ptAO
    closes #27

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import ptAO from '@angular/common/locales/pt-AO';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from '@studio/core';
 import { QuicklinkModule } from 'ngx-quicklink';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+registerLocaleData(ptAO);
 
 @NgModule({
   declarations: [ AppComponent ],
@@ -13,7 +17,9 @@ import { AppComponent } from './app.component';
     QuicklinkModule,
     CoreModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-AO' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
