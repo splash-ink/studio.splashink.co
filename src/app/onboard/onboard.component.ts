00cfbcddec09e63555a9f0b381f715a13ecef363
Jorge Dacosta <jorgeMSDC@mail.com>
2020-07-30 15:44:42 +0100

    improvement(onboard): generate SEO onInit

--- src/app/onboard/onboard.component.ts
+++ src/app/onboard/onboard.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SeoService } from '@studio/core';
 
 @Component({
   templateUrl: './onboard.component.html',
   styleUrls: ['./onboard.component.css']
 })
-export class OnboardComponent {
+export class OnboardComponent implements OnInit {
 
-  constructor() {}
+  constructor(private readonly seoService: SeoService) {}
+
+  ngOnInit(): void {
+    this.seoService.generateTags({
+      title: 'Splash Ink - Estúdio Digital',
+      description: 'Ajustamos tudo para que minimize seus custos com preços bem definidos e aproveite dos nossos serviços.',
+      image: 'https://domain-to-picture.com',
+    });
+  }
 
 }
