0c43f3601256fe4594733f881b8cc55241b13a7a
Jorge Dacosta <jorgeMSDC@mail.com>
2020-07-30 13:58:00 +0100

    improvement(gallery): generate SEO onInit

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SeoService } from '@studio/core';
 import * as $ from 'jquery';
 import 'magnific-popup';
-import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 
 
 @Component({
-  selector: 'ds-gallery',
   templateUrl: './gallery.component.html',
-  styles: []
 })
 export class GalleryComponent implements OnInit, AfterViewInit {
 
-  id;
-  constructor(private route: ActivatedRoute) { }
+  private id: string | number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private readonly seoService: SeoService
+  ) { }
 
   ngOnInit() {
     this.route.paramMap.pipe(
       switchMap(params => this.id = params.get('id'))
     );
+
+    this.seoService.generateTags({
+      title: `Afrikaneeze - Álbum`,
+      description: 'Ajustamos tudo para que minimize seus custos com preços bem definidos e aproveite dos nossos serviços.',
+      image: 'https://domain-to-picture.com',
+    });
   }
 
   ngAfterViewInit() {
