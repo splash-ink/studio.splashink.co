b106ff36a2845683ae21be371b58db4e10830b40
Jorge Dacosta <jorgeMSDC@mail.com>
2020-09-14 14:12:45 +0100

    improvement(contact-us): import shared module

--- src/app/contact-us/contact-us.module.ts
+++ src/app/contact-us/contact-us.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { SharedModule } from '@studio/shared';
 import { ContactUsRoutingModule } from './contact-us-routing.module';
 import { ContactUsComponent } from './contact-us.component';
-
 
 @NgModule({
   declarations: [ContactUsComponent],
   imports: [
     CommonModule,
     ContactUsRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class ContactUsModule { }
