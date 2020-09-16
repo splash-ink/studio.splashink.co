6cef630178e4adecd8f8d014aeab92f8d2d4a0bb
Jorge Dacosta <jorgeMSDC@mail.com>
2020-09-16 13:57:39 +0100

    feat(galleries): import shared module utils

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { GalleryRoutingModule } from './gallery-routing.module';
 import { GalleryComponent } from './gallery.component';
 import { GalleriesComponent } from './galleries/galleries.component';
+import { SharedModule } from '@studio/shared';
 
 @NgModule({
   declarations: [GalleryComponent, GalleriesComponent],
   imports: [
     CommonModule,
-    GalleryRoutingModule
+    GalleryRoutingModule,
+    SharedModule
   ]
 })
 export class GalleryModule { }
