ccd484c2ec27c25555f6c135c9f10d146d0c9373
Jorge Dacosta <jorgeMSDC@mail.com>
2021-01-15 15:49:12 +0100

    fix(onboard): set empty obj to generateTags to extends default

--- src/app/onboard/onboard.component.ts
+++ src/app/onboard/onboard.component.ts
@@ -10,11 +10,7 @@ export class OnboardComponent implements OnInit {
   constructor(private readonly seoService: SeoService) {}
 
   ngOnInit(): void {
-    this.seoService.generateTags({
-      title: 'Splash Ink - Estúdio Digital',
-      description: 'Ajustamos tudo para que minimize seus custos com preços bem definidos e aproveite dos nossos serviços.',
-      image: 'https://domain-to-picture.com',
-    });
+    this.seoService.generateTags({});
   }
 
 }
